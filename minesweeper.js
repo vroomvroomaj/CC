34958004a92bcf54748e7e2e8fb442a1b3702736
Ajay Bandi <ajay.bandi@sfmotors.local>
2018-05-22 17:33:47 -0700

    removed commented code

--- minesweeper.js
+++ minesweeper.js
@@ -1,14 +1,3 @@
-/*const blankLine = '  |   |  ';
-console.log(`This is what an empty board would look like:`);
-console.log(`${blankLine}`);
-console.log(`${blankLine}`);
-console.log(`${blankLine}`);
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
-console.log(`This is what a board with a guess and a bomb on it would look like:`)
-console.log(`${guessLine}`);
-console.log(`${bombLine}`);
-console.log(`${blankLine}`);*/
 const printBoard = (board) => {
   console.log(`Current Board:`);
   console.log(board[0].join(' | '));
