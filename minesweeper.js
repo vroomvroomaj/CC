179de1f60ac27f2870919a05313e2c8ac1ba34c7
Ajay Bandi <ajay.bandi@sfmotors.local>
2018-06-01 11:28:32 -0700

    Adding day 12 progress

--- minesweeper.js
+++ minesweeper.js
@@ -1,16 +1,40 @@
-const printBoard = (board) => {
-  console.log(`Current Board:`);
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
 };
-let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
-//console.log(board);
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+console.log(generatePlayerBoard(10, 10));
+
+
+//Creating Bomb board generator
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced != numberOfBombs) {
+    //An updated printBoard() Function
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+  }
+  return board;
+};
+console.log(generateBombBoard(10, 10, 100));
+
+//Function to print boards to console
+const printBoard = (board) => {
+  board.map()
+};
